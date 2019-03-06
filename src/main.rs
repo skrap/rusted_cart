b9965d0fb033e59b7517cfd9e633b687729aa0ee
Jonah Petri <jonah@petri.us>
2019-03-06 07:32:52 -0500

    Checkpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
 use std::error::Error;
 use std::iter::FromIterator;
+use permutator::Combination;
+use std::collections::BTreeSet;
 
 fn mushes() -> Result<Vec<Mush>, Box<Error>> {
     let f = std::fs::File::open("assets/agaricus-lepiota.data")?;
@@ -12,8 +14,6 @@ fn mushes() -> Result<Vec<Mush>, Box<Error>> {
     }
     Ok(result)
 }
-
-use std::collections::BTreeSet;
 
 fn main() {
     let mush = mushes().expect("error getting mushy");
@@ -30,28 +30,32 @@ fn main() {
         let best_q = questions
             .map(|q| q.answer(&mush))
             .max_by(|m1, m2| m1.impurity().partial_cmp(&m2.impurity()).unwrap());
+        dbg!(best_q);
     }
 }
 
-struct Questions<'a> {
+struct Questions<'a, I> where I : Iterator {
     mush: &'a [Mush],
     facet: usize,
 }
 
 impl<'a> Questions<'a> {
     fn new(mush: &'a [Mush]) -> Questions {
-        Questions { mush, facet: 0 }
+        Questions { 
+            mush, 
+            facet: 0,
+        }
     }
 }
 
 impl<'a> Iterator for Questions<'a> {
     type Item = Question;
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        self.
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Question {
     facet: usize,
     vals: BTreeSet<char>,
@@ -104,6 +108,7 @@ fn test_answer() {
     assert_eq!(a.no.len(), 2);
 }
 
+#[derive(Debug)]
 struct Answer {
     question: Question,
     yes: Vec<Mush>,
@@ -111,17 +116,17 @@ struct Answer {
 }
 
 impl Answer {
+    fn gini_two_class_impurity(yes: &[Mush], no: &[Mush]) -> f64 {
+        let gini = |v: &[Mush]| {
+            let p = v.iter().filter(|m| m.poison == 'p').count() as f64;
+            let l = v.len() as f64;
+            2.0 * (p / l) * ((l - p) / l)
+        };
+        gini(yes) + gini(no)
+    }
+
     fn impurity(&self) -> f64 {
-        // gini 2-class
-        let yp1t =
-            self.yes.iter().filter(|m| m.poison == 'p').count() as f64 / self.yes.len() as f64;
-        let yp2t =
-            self.yes.iter().filter(|m| m.poison != 'p').count() as f64 / self.yes.len() as f64;
-
-        let np1t = self.no.iter().filter(|m| m.poison == 'p').count() as f64 / self.no.len() as f64;
-        let np2t = self.no.iter().filter(|m| m.poison != 'p').count() as f64 / self.no.len() as f64;
-
-        yp1t * yp2t + np1t * np2t
+        Answer::gini_two_class_impurity(&self.yes, &self.no)
     }
 }
 
@@ -141,11 +146,23 @@ fn test_impurity() {
         .collect();
     let mixed: Vec<_> = (0..10)
         .map(|i| Mush {
-            poison: if i % 2 == 0 {'e'} else {'p'},
+            poison: if i % 2 == 0 { 'e' } else { 'p' },
             attrs: ['a'; 22],
         })
         .collect();
-    
+
+    assert!(
+        Answer::gini_two_class_impurity(&poisons, &edibles)
+            < Answer::gini_two_class_impurity(&poisons, &mixed)
+    );
+    assert!(
+        (Answer::gini_two_class_impurity(&poisons, &mixed)
+            - Answer::gini_two_class_impurity(&mixed, &poisons)).abs() < std::f64::EPSILON;
+    );
+    assert!(
+        Answer::gini_two_class_impurity(&edibles, &poisons)
+            < Answer::gini_two_class_impurity(&poisons, &mixed)
+    );
 }
 
 struct Node {
